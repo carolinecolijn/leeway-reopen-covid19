fbd2f705e311f193ad123c9a2bc77fd3160e2aa0
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2020-05-21 14:13:45 -0700

    Belgium with general priors.
    Former-commit-id: 787b61a5ecc34266880a36bf6e0743c6628758ec

--- 01-fit-belgium.R
+++ 01-fit-belgium.R
@@ -38,10 +38,12 @@ fit_file <- file.path(this_folder, "data-generated/BE-fit.rds")
 if (!file.exists(fit_file)) {
   fit <- covidseir::fit_seir(
     daily_cases = dat$daily_cases,
-    samp_frac_fixed = rep(0.2, nrow(dat)),
-    i0_prior = c(log(1), 1),
-    start_decline_prior = c(log(9), 0.2),
-    end_decline_prior = c(log(17), 0.2),
+    samp_frac_fixed = rep(SAMP_FRAC, nrow(dat)),
+    i0_prior = i0_PRIOR,
+    start_decline_prior = c(log(get_google_start("Belgium", dat)), 0.2),
+                                            # = log(8), original was log(9)
+    end_decline_prior = c(log(get_google_end("Belgium", dat)), 0.2),
+                                            # = log(18), original was log(17)
     N_pop = 11476279,
     chains = CHAINS,
     iter = ITER
