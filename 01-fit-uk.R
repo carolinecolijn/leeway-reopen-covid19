ca365b6a526c42b7098c043f00b3b75fdd590e18
Sean Anderson <sean@seananderson.ca>
2020-05-21 13:36:38 -0700

    Update 01-fit-uk.R
    Former-commit-id: d4b830b40f30ce6ffe704e325e4a9e8b4677e621

--- 01-fit-uk.R
+++ 01-fit-uk.R
@@ -1,5 +1,5 @@
 # Load some packages, functions, and global variables:
-source(here::here("selfIsolationModel/contact-ratios/model-prep.R"))
+source("selfIsolationModel/contact-ratios/model-prep.R")
 
 # Notes ---------------------------------------------------------------------
 
@@ -17,14 +17,16 @@ source(here::here("selfIsolationModel/contact-ratios/model-prep.R"))
 
 # Read and prepare data -----------------------------------------------------
 
-#dat <- readr::read_csv("https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-totals-uk.csv")
-dat <- readr::read_csv(here(this_folder,"data-raw/UK.csv"))
+# dat <- readr::read_csv("https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-totals-uk.csv")
+dat <- readr::read_csv(here(this_folder, "data-raw/UK.csv"))
 dat <- rename(dat, date = Date)
-dat$cases <- c(dat$ConfirmedCases[1],diff(dat$ConfirmedCases))
-dat$daily_tests <- c(dat$Tests[1],diff(dat$Tests))
+dat$cases <- c(dat$ConfirmedCases[1], diff(dat$ConfirmedCases))
+dat$daily_tests <- c(dat$Tests[1], diff(dat$Tests))
 
-n<-dim(dat)[1]
-if (is.na(tail(dat$cases, n=1))){dat<-dat[1:(n-1),]}
+n <- dim(dat)[1]
+if (is.na(tail(dat$cases, n = 1))) {
+  dat <- dat[1:(n - 1), ]
+}
 # gives NAs if the deaths have been released today but not the cases yet
 
 # View(dat)
@@ -39,14 +41,14 @@ ggplot(dat, aes(date, cases)) +
 
 # Try redistributing Apr 11th bump:
 dat$adjust_cases <- dat$cases
-excess <- (dat[42,]$cases - dat[43,]$cases)/5
-dat[42,]$adjust_cases <- dat[43,]$cases
-dat[37:41,]$adjust_cases <- round(dat[37:41,]$cases + excess)
+excess <- (dat[42, ]$cases - dat[43, ]$cases) / 5
+dat[42, ]$adjust_cases <- dat[43, ]$cases
+dat[37:41, ]$adjust_cases <- round(dat[37:41, ]$cases + excess)
 dat$value <- dat$adjust_cases # for plotting function
 ggplot(dat, aes(day, value)) +
   geom_point()
 
-saveRDS(dat, here(this_folder, "data-generated/UK-dat.rds"))
+saveRDS(dat, file.path(this_folder, "data-generated/UK-dat.rds"))
 
 # Fit model -----------------------------------------------------------------
 
@@ -54,45 +56,49 @@ saveRDS(dat, here(this_folder, "data-generated/UK-dat.rds"))
 # x <- seq(0, 10, length.out = 200)
 # plot(x, dlnorm(x, log(1), 0.5), type = "l", xaxs = "i", yaxs = "i")
 
-fit <- covidseir::fit_seir(
-  daily_cases = dat$value,
-  samp_frac_fixed = c(rep(0.2,  60), rep(0.3,  nrow(dat)-60)),
-  # incorporating testing ramped up 30/04 = day 61
-  i0_prior = c(log(1), 0.5),
-  start_decline_prior = c(log(16), 0.1),
-  end_decline_prior = c(log(23), 0.1),
-  N_pop = 66.4e6,
-  chains = 4,
-  iter = 300
-)
-
+fit_file <- file.path(this_folder, "data-generated/UK-fit.rds")
+if (!file.exists(fit_file)) {
+  fit <- covidseir::fit_seir(
+    daily_cases = dat$value,
+    samp_frac_fixed = c(rep(0.2, 60), rep(0.3, nrow(dat) - 60)),
+    # incorporating testing ramped up 30/04 = day 61
+    i0_prior = c(log(1), 1),
+    start_decline_prior = c(log(16), 0.1),
+    end_decline_prior = c(log(23), 0.1),
+    N_pop = 66.4e6,
+    chains = CHAINS,
+    iter = ITER
+  )
+  saveRDS(fit, fit_file)
+} else {
+  fit <- readRDS(fit_file)
+}
 print(fit)
 make_traceplot(fit)
-saveRDS(fit, here(this_folder, "data-generated/UK-fit.rds"))
-
-# Check fit -----------------------------------------------------------------
-
-proj <- covidseir::project_seir(fit, iter = 1:50, forecast_days = 30)
-proj_tidy <- covidseir::tidy_seir(proj)
-
-proj_tidy %>%
-  covidseir::plot_projection(dat)
-
-proj_tidy %>%
-  covidseir::plot_projection(dat) +
-  scale_y_log10()
-
-# Calculate threshold for increase ------------------------------------------
-
-# Need to pick reasonable f(s) values for a reasonable time span
-# such that fitting a linear regression makes sense.
-# Make sure the plot that comes out of this is linear:
-threshold <- get_thresh(fit, iter = 1:50,
-  forecast_days = 30, fs = seq(0.1, 0.7, length.out = 5))
-round(threshold, 2)
-saveRDS(threshold, here(this_folder, "data-generated/UK-threshold.rds"))
-
-# Quick plot:
-hist(fit$post$f_s[,1],
-  main = "", xlab = "Estimated fraction of normal contacts", breaks = 20)
-abline(v = threshold, col = "red", lwd = 2)
+#
+# # Check fit -----------------------------------------------------------------
+#
+# proj <- covidseir::project_seir(fit, iter = 1:50, forecast_days = 30)
+# proj_tidy <- covidseir::tidy_seir(proj)
+#
+# proj_tidy %>%
+#   covidseir::plot_projection(dat)
+#
+# proj_tidy %>%
+#   covidseir::plot_projection(dat) +
+#   scale_y_log10()
+#
+# # Calculate threshold for increase ------------------------------------------
+#
+# # Need to pick reasonable f(s) values for a reasonable time span
+# # such that fitting a linear regression makes sense.
+# # Make sure the plot that comes out of this is linear:
+# threshold <- get_thresh(fit, iter = 1:50,
+#   forecast_days = 30, fs = seq(0.1, 0.7, length.out = 5))
+# round(threshold, 2)
+# saveRDS(threshold, here(this_folder, "data-generated/UK-threshold.rds"))
+#
+# # Quick plot:
+# hist(fit$post$f_s[,1],
+#   main = "", xlab = "Estimated fraction of normal contacts", breaks = 20)
+# abline(v = threshold, col = "red", lwd = 2)
