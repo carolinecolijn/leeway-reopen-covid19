4295dbdc6e010fa5d61f5a2e91e084fd477ad1f0
Sean Anderson <sean@seananderson.ca>
2020-05-24 13:16:09 -0700

    Add Germany; rename Denmark code
    Former-commit-id: 406964b076898611cd6460520966415d6375a169

---
+++ 01-fit-germany.R
@@ -0,0 +1,64 @@
+source("selfIsolationModel/contact-ratios/model-prep.R")
+RawData <- readr::read_csv(file.path(this_folder,"data-raw/EURO.csv"))
+RawData$date <- lubridate::ymd(RawData$dateRep)
+GermanyRaw <- dplyr::filter(RawData, countriesAndTerritories == "Germany")
+
+dat <- GermanyRaw %>%
+  dplyr::arrange(date) %>%
+  dplyr::select(date, cases) %>%
+  rename(value = cases)
+
+ggplot(dat, aes(date, value)) +
+  geom_point()
+
+dat <- dat %>%
+  mutate(daily_cases_smooth = zoo::rollmean(value, k = 3, fill = NA))
+
+ggplot(dat, aes(date, value)) +
+  geom_point() +
+  geom_line(aes(y = daily_cases_smooth))
+
+# Pick a reasonable starting date:
+origin <- lubridate::ymd("2020-03-01")
+dat <- dplyr::filter(dat, date >= origin)
+dat$day <- seq_len(nrow(dat))
+
+dat$value <- dat$daily_cases_smooth
+ggplot(dat, aes(date, value)) +
+  geom_point()
+
+saveRDS(dat, file.path(this_folder, "data-generated/DE-dat.rds"))
+
+fit_file <- file.path(this_folder, "data-generated/DE-fit.rds")
+if (!file.exists(fit_file)) {
+  fit <- covidseir::fit_seir(
+    daily_cases = dat$value,
+    samp_frac_fixed = rep(SAMP_FRAC, nrow(dat)),
+    i0_prior = i0_PRIOR,
+    start_decline_prior = c(log(get_google_start("Germany", dat)), 0.1),
+    end_decline_prior = c(log(get_google_end("Germany", dat)), 0.1),
+    f_seg = make_f_seg(dat),
+    N_pop = 83e6,
+    # https://www.destatis.de/EN/Themes/Society-Environment/Population/Current-Population/_node.html
+    chains = CHAINS,
+    iter = ITER
+  )
+  saveRDS(fit, fit_file)
+} else {
+  fit <- readRDS(fit_file)
+}
+
+print(fit)
+make_traceplot(fit)
+
+# Check fit -----------------------------------------------------------------
+
+# proj <- covidseir::project_seir(fit, iter = 1:50, forecast_days = 30)
+# proj_tidy <- covidseir::tidy_seir(proj)
+#
+# proj_tidy %>%
+# 	covidseir::plot_projection(DenmarkData)
+#
+# proj_tidy %>%
+# 	covidseir::plot_projection(DenmarkData) +
+# 	scale_y_log10()
