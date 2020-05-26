3b7d88ebbfa7f77a4fb3dc69eab20fd102e4b2a9
Sean Anderson <sean@seananderson.ca>
2020-05-25 18:52:32 -0700

    Bumper required version
    Former-commit-id: 12f54249de5c87ea838adf40d32bef54d628987f

--- model-prep.R
+++ model-prep.R
@@ -4,8 +4,8 @@ library(covidseir)
 library(purrr)
 library(future)
 
-if (packageVersion("covidseir") != "0.0.0.9003") {
-  stop("packageVersion('covidseir') != '0.0.0.9003'. Please install the latest version of the model:\n`devtools::install_github('seananderson/covidseir')`")
+if (packageVersion("covidseir") != "0.0.0.9004") {
+  stop("packageVersion('covidseir') != '0.0.0.9004'. Please install the latest version of the model:\n`devtools::install_github('seananderson/covidseir')`")
 }
 
 wd <- getwd()
