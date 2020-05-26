9b58d6f2d8c90b6a57bd98afa7390fdae9e98823
Sean Anderson <sean@seananderson.ca>
2020-05-26 00:23:39 -0700

    Add exploratory figures
    Former-commit-id: 36c4ed93e5b52388df6bb80699836c2a4f5c35fc

--- 05-importations.R
+++ 05-importations.R
@@ -6,13 +6,14 @@ set.seed(12893)
 ITER_PROJ <- sample(seq_len(N_ITER), 100)
 mults <- c(1.0, 1.5, 2.0) %>% set_names()
 imports <- c(0, 10) %>% set_names()
+round_time <- seq_len(500)
 
 future::plan(future::multisession)
 projections_imp <-
   map_dfr(mults, function(.mult) {
     cat(.mult, "\n")
     furrr::future_map2_dfr(fits, observed_data, function(.fit, .obs) {
-      map_dfr(imports, function(.import) {
+      out <- map_dfr(imports, function(.import) {
         days <- length(.obs$day)
         covidseir::project_seir(
           obj = .fit,
@@ -26,16 +27,60 @@ projections_imp <-
           return_states = TRUE
         )
       }, .id = "import")
+      dplyr::filter(out, time %in% round_time, variable %in% c("R", "Rd", "E2")) # save space
     }, .id = "region")
   }, .id = "f_multi")
 future::plan(future::sequential)
-saveRDS(projections_imp, file = file.path(dg_folder, "projections-multi-imp.rds"))
-projections_imp <- readRDS(file.path(dg_folder, "projections-multi-imp.rds"))
+saveRDS(projections_imp, file = file.path(dg_folder, "projections-multi-imp1.rds"))
+projections_imp <- readRDS(file.path(dg_folder, "projections-multi-imp1.rds"))
+
+R <- projections_imp %>%
+  filter(time %in% round_time) %>%
+  filter(variable %in% c("R", "Rd")) %>%
+  group_by(f_multi, region, import, .iteration, time) %>%
+  summarise(cases = value[variable == "R"] + value[variable == "Rd"])
+
+E2 <- projections_imp %>%
+  filter(time %in% round_time) %>%
+  filter(variable %in% c("E2")) %>%
+  group_by(f_multi, region, import, .iteration, time) %>%
+  summarise(cases = value[variable == "E2"])
+
+E2 %>%
+  filter(import == "0") %>%
+  ggplot(aes(time, cases, colour = f_multi, group = paste(.iteration, f_multi))) +
+  geom_line(alpha = 0.3) +
+  facet_wrap(~region, scales = "free_y") +
+  theme_light()
+
+R %>%
+  filter(import == "0", region == "JP") %>%
+  ggplot(aes(time, cases, colour = f_multi, group = paste(.iteration, f_multi))) +
+  geom_line(alpha = 0.3) +
+  facet_wrap(~region, scales = "free_y") +
+  theme_light()
+
+R %>%
+  filter(import == "10", region == "JP") %>%
+  ggplot(aes(time, cases, colour = f_multi, group = paste(.iteration, f_multi))) +
+  geom_line(alpha = 0.3) +
+  facet_wrap(~region, scales = "free_y") +
+  theme_light()
+
+g <- R %>%
+  group_by(time, f_multi, region, .iteration) %>%
+  summarise(new_cases = cases[import == "10"] - cases[import == "0"]) %>%
+  ggplot(aes(time, new_cases, colour = f_multi, group = paste(.iteration, f_multi))) +
+  geom_line(alpha = 0.3) +
+  facet_wrap(~region, scales = "free_y") +
+  theme_light() +
+  ylab("New R + Rd")
+ggsave(file.path(fig_folder, "proj-new-cases.png"), width = 12, height = 9)
 
 import_cases <- projections_imp %>%
-  dplyr::filter(variable %in% c("R", "Rd")) %>%
+  filter(variable %in% c("R", "Rd")) %>%
   group_by(f_multi, region, import, .iteration) %>%
-  dplyr::filter(time == max(time)) %>%
+  filter(time == max(time)) %>%
   summarise(cases = value[variable == "R"] + value[variable == "Rd"]) %>%
   group_by(f_multi, region, .iteration) %>%
   summarise(new_cases = cases[import == "10"] - cases[import == "0"])
@@ -44,7 +89,8 @@ import_cases %>%
   ggplot(aes(new_cases, fill = f_multi)) +
   geom_histogram(position = "identity", alpha = 0.5) +
   facet_wrap(~region, scales = "free_x") +
-  scale_x_log10()
+  # scale_x_log10() +
+  theme_light()
 
 ggsave(file.path(fig_folder, "hist-imports.pdf"), width = 9, height = 9)
 ggsave(file.path(fig_folder, "hist-imports.png"), width = 9, height = 9)
