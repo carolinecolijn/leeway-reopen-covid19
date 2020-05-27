98d706072349a2fa8df9c5aae71dc4bf72122fb4
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2020-05-27 15:21:16 -0700

    Table - further values automated from output.
    Former-commit-id: c5a47f6a9533c04a37e34d77b8e0e4fefc5d2eeb

--- 10-manuscript-tables.R
+++ 10-manuscript-tables.R
@@ -1,43 +1,56 @@
 # code to automatically generate tables for parameters etc. for each
-# jurisdiction, for Supplementary Table for how-safe-is-reopening manuscript
+# jurisdiction, for Supplementary Table for how-safe-is-reopening manuscript.
+# Delay shape and scale are not automatic, but taken from the input files
 source("selfIsolationModel/contact-ratios/model-prep.R")
 source("selfIsolationModel/contact-ratios/projection-prep.R")
 
 # observed_data is a list of tibbles, one for each region (jurisdiction)
 # fits is a list of fits, which are covidseir objects with all the info
 
-# Here are what's saved, removing ones I've used or aren't needed
-summary(fits$BC)
-                     Length Class   Mode
-fit                    1    stanfit S4
-post                  11    -none-  list
-phi_prior              1    -none-  numeric
-f_prior                2    -none-  numeric
-obs_model              1    -none-  numeric
-e_prior_trans          2    -none-  numeric
-start_decline_prior    2    -none-  numeric
-end_decline_prior      2    -none-  numeric
-samp_frac_fixed       77    -none-  numeric
-state_0               10    -none-  numeric
-daily_cases           77    -none-  numeric
-days                  77    -none-  numeric
-time                 429    -none-  numeric
-last_day_obs           1    -none-  numeric
-pars                   9    -none-  numeric
-stan_data             36    -none-  list
-days_back              1    -none-  numeric
->
+# Here are what's saved:
+# summary(fits$BC)
+#                      Length Class   Mode
+# fit                    1    stanfit S4
+# post                  11    -none-  list
+# phi_prior              1    -none-  numeric
+# R0_prior               2    -none-  numeric
+# f_prior                2    -none-  numeric
+# obs_model              1    -none-  numeric
+# e_prior_trans          2    -none-  numeric
+# start_decline_prior    2    -none-  numeric
+# end_decline_prior      2    -none-  numeric
+# samp_frac_fixed       77    -none-  numeric
+# state_0               10    -none-  numeric
+# daily_cases           77    -none-  numeric
+# days                  77    -none-  numeric
+# time                 429    -none-  numeric
+# last_day_obs           1    -none-  numeric
+# pars                   9    -none-  numeric
+# f2_prior_beta_shape1   1    -none-  numeric
+# f2_prior_beta_shape2   1    -none-  numeric
+# stan_data             36    -none-  list
+# days_back              1    -none-  numeric
 
-# If order of Details changes then also need to change order in the loop
+# If order of Details column changes then also need to change order in the loop, this
 details <- tibble::tibble("Detail" = c("Data start",
                                        "Data end",
                                        "$I_0$",     # incidence 30 days before
                                                     # day 1 (or 0?).
                                        "$f_2$ prior: Beta(mean, sd)",
-                                       "$R_{\\mathrm{0b}}$ prior: Lognormal(logmean, sd)"
+                                       "$R_{\\mathrm{0b}}$ prior: Lognormal(logmean, sd)",
+                                       "$1/\\sqrt \\phi$ prior: sd (**check)",
+                                       "$f$ prior: ?????",
+                                       "$f_2$ ramp start prior mean",
+                                       "$f_2$ ramp end prior mean",
+                                       "$e$: proportion distancing (transformed prior**)",
+                                       "Delay shape",
+                                       "Delay scale",
+                                       "Sampling fraction(s)**",
+                                       "$N$: population (millions)"
                                        )
                           )
 
+# Fill in a column for each REGION
 for(i in 1:length(REGIONS)){
   details <- details %>%
     dplyr::mutate(!!REGIONS[i] := c(
@@ -55,9 +68,29 @@ for(i in 1:length(REGIONS)){
                              fits[[i]]$f2_prior_beta_shape2),
                       paste0(round(fits[[i]]$R0_prior[1], 2),
                              ", ",
-                             round(fits[[i]]$R0_prior[2], 2))
+                             round(fits[[i]]$R0_prior[2], 2)),
+                      fits[[i]]$phi_prior,
+                      paste0(fits[[i]]$f_prior[1], ", ", fits[[i]]$f_prior[2]),
+                      paste(lubridate::month(exp(fits[[i]]$start_decline_prior)[1]
+                                             + min(observed_data[[i]]$date),
+                                             label = TRUE,
+                                             abbr = TRUE),
+                            lubridate::day(exp(fits[[i]]$start_decline_prior)[1]
+                                           + min(observed_data[[i]]$date))),
+                      paste(lubridate::month(exp(fits[[i]]$end_decline_prior)[1]
+                                             + min(observed_data[[i]]$date),
+                                             label = TRUE,
+                                             abbr = TRUE),
+                            lubridate::day(exp(fits[[i]]$end_decline_prior)[1]
+                                           + min(observed_data[[i]]$date))),
+                      paste0(fits[[i]]$e_prior_trans[1],
+                             ", ",
+                             fits[[i]]$e_prior_trans[2]),
+                      ifelse(REGIONS[i] == "NZ", 1.53, 1.73),
+                      ifelse(REGIONS[i] == "NZ", 7.83, 9.85),
+                      paste(unique(fits[[i]]$samp_frac), collapse=", "),
+                      sprintf("%.2f", fits[[i]]$pars["N"]/1e6, 2)
                     ))
-
 }
 
 as.data.frame(details)
@@ -66,4 +99,5 @@ kableExtra::kable(details,
                   format = "latex",
                   booktabs = TRUE,
                   escape = FALSE) %>%
-  kableExtra::column_spec(1, width="30mm")
+  kableExtra::column_spec(1, width="40mm")
+# Then paste that into .tex on Overleaf
