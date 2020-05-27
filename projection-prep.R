ffdba3414802db124c7e9169597e49a8549677bc
Sean Anderson <sean@seananderson.ca>
2020-05-27 12:16:20 -0700

    Add to region lookup
    Former-commit-id: 37b7cd6565844b4149dbabb473f7cdd65610099a

--- projection-prep.R
+++ projection-prep.R
@@ -2,7 +2,7 @@ dg_folder <- "selfIsolationModel/contact-ratios/data-generated/"
 fig_folder <- "selfIsolationModel/contact-ratios/figs/"
 dir.create(dg_folder, showWarnings = FALSE)
 dir.create(fig_folder, showWarnings = FALSE)
-REGIONS <- c("BC", "BE", "CA", "DE", "FL", "MI", "NY", "NZ", "ON", "QC", "UK", "WA", "SWE", "JP")
+REGIONS <- c("BC", "BE", "CA", "DE", "NY", "NZ", "ON", "QC", "UK", "WA", "SWE", "JP")
 REGIONS <- sort(REGIONS)
 N_ITER <- CHAINS * ITER / 2
 PROJ_ITER <- 100
@@ -28,20 +28,19 @@ observed_data <- map(seq_along(observed_data), function(.x) {
 # observed_data
 
 country_lookup <- tibble::tribble(
-  ~region, ~region_group,
-  "BC", "CAN",
-  "BE", "EUR",
-  "CA", "US",
-  "DE", "EUR",
-  # "DK", "EUR",
-  "JP", "PAC",
-  "FL", "US",
-  "MI", "US",
-  "NY", "US",
-  "NZ", "PAC",
-  "ON", "CAN",
-  "QC", "CAN",
-  "SWE", "EUR",
-  "UK", "EUR",
-  "WA", "US"
+  ~region, ~region_group, ~region_long, ~country,
+  "BC", "CAN", "British Columbia", "CAN",
+  "BE", "EUR", "Belgium", "BE",
+  "CA", "US", "California", "US",
+  "DE", "EUR", "Denmark", "DE",
+  "JP", "PAC", "Japan", "JP",
+  "FL", "US", "Florida", "US",
+  "MI", "US", "Michigan", "US",
+  "NY", "US", "New York", "US",
+  "NZ", "PAC", "New Zealand", "NZ",
+  "ON", "CAN", "Ontario", "CAN",
+  "QC", "CAN", "Québec", "CAN",
+  "SWE", "EUR", "Sweden", "SWE",
+  "UK", "EUR", "United Kingdom", "UK",
+  "WA", "US", "Washington", "US"
 )
