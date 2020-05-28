8208c7b3f71b3010550856a0430e60e78b7a9aaf
Sean Anderson <sean@seananderson.ca>
2020-05-28 13:55:00 -0700

    Update mobility plots and data
    Former-commit-id: c948d80c3b5b432802866640a98d368312b98c59

--- 00-mobility-segments.R
+++ 00-mobility-segments.R
@@ -9,6 +9,9 @@ library(purrr)
 # Analyze google mobility data
 # dat is data to be fitted
 # should have transit data & dates
+
+mobility_regions <- c("British Columbia", "New York", "Sweden", "Germany", "Quebec", "Washington", "New Zealand",  "Ontario", "Belgium", "United Kingdom", "California", "Japan")
+
 get_sd_ramp_dates <- function(dat, region, return_plots = FALSE) {
   #---Google data------------------#
   dat <- dat %>%
@@ -34,7 +37,10 @@ get_sd_ramp_dates <- function(dat, region, return_plots = FALSE) {
     ggtitle(region) +
     geom_line(data = dat, aes(x = date, y = transitMean), alpha = 0.4) +
     geom_line(data = dat.model, aes(x = date, y = change)) +
-    theme_light()
+    # theme_light()
+    ggsidekick::theme_sleek() +
+    ylab("Mean transit value") + theme(axis.title.x = element_blank()) +
+    ylim(-95, 15)
 
   # ggsave(here::here(paste0("plots/", region, "_plot.pdf")),
   #     plot = plt,
@@ -59,9 +65,9 @@ if (!file.exists(file2)) {
     "https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv?cachebust=722f3143b586a83f")
   saveRDS(g, file =  file)
   g$date <- lubridate::ymd(g$date)
-  regions <- c("British Columbia", "North Carolina", "New York", "France", "Spain", "Australia", "Michigan", "Sweden", "Denmark", "Germany", "Alberta", "Quebec", "Washington", "New Zealand", "Florida", "Ontario", "Belgium", "United Kingdom", "California", "Japan")
-  g <- dplyr::filter(g, country_region %in% regions | sub_region_1 %in% regions)
-  g <- g %>% dplyr::mutate(region = ifelse(country_region %in% regions, country_region, sub_region_1))
+
+  g <- dplyr::filter(g, country_region %in% mobility_regions | sub_region_1 %in% mobility_regions)
+  g <- g %>% dplyr::mutate(region = ifelse(country_region %in% mobility_regions, country_region, sub_region_1))
   g <- dplyr::select(g, region, transit_stations_percent_change_from_baseline, date)
   saveRDS(g, file = file2)
 } else {
@@ -70,6 +76,7 @@ if (!file.exists(file2)) {
 
 #----PLOT & DATES----------#
 g_dates <- g %>%
+  filter(region %in% mobility_regions) %>%
   group_by(region) %>%
   tidyr::nest() %>%
   mutate(rampDates = map(data, get_sd_ramp_dates, region = region)) %>%
@@ -78,13 +85,15 @@ g_dates <- g %>%
 print(g_dates)
 
 g_plots <- g %>%
+  filter(region %in% mobility_regions) %>%
   group_by(region) %>%
   group_split() %>%
   purrr::map(~ get_sd_ramp_dates(dat = .x, region = unique(.x$region), return_plots = TRUE))
+# names(g_plots) <- mobility_regions
 
 plots <- cowplot::plot_grid(plotlist = g_plots)
-ggsave("selfIsolationModel/contact-ratios/figs/google-plots.pdf", width = 10, height = 9)
-ggsave("selfIsolationModel/contact-ratios/figs/google-plots.png", width = 10, height = 9)
+ggsave("selfIsolationModel/contact-ratios/figs/google-plots.pdf", width = 9, height = 5.5)
+ggsave("selfIsolationModel/contact-ratios/figs/google-plots.png", width = 8, height = 7)
 
 g_dates %>%
   rename(start_date = startDate, end_date = endDate) %>%
