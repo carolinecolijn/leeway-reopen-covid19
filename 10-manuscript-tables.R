d76f683507312f69c1bfdaaaa265e978eb98bbe9
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2020-05-28 12:22:15 -0700

    Table - move f to other table, don't need f2.
    Former-commit-id: bf5199f4e3fe58d126e2bb7f1786f6e30cfc6732

--- 10-manuscript-tables.R
+++ 10-manuscript-tables.R
@@ -34,9 +34,7 @@ source("selfIsolationModel/contact-ratios/projection-prep.R")
 # If order of Details column changes then also need to change order in the loop, this
 details <- tibble::tibble("Detail" = c("Data start",
                                        "Data end",
-                                       "$f_2$ prior: Beta(mean, sd)",
                                        "$1/\\sqrt \\phi$ prior: sd (**check)",
-                                       "$f$ prior: ?????",
                                        "$f_2$ ramp start prior mean",
                                        "$f_2$ ramp end prior mean",
                                        "$e$: proportion distancing (transformed prior**)",
@@ -59,11 +57,7 @@ for(i in 1:length(REGIONS)){
                                              label = TRUE,
                                              abbr = TRUE),
                             lubridate::day(max(observed_data[[i]]$date))),
-                      paste0(fits[[i]]$f2_prior_beta_shape1,
-                             ", ",
-                             fits[[i]]$f2_prior_beta_shape2),
                       fits[[i]]$phi_prior,
-                      paste0(fits[[i]]$f_prior[1], ", ", fits[[i]]$f_prior[2]),
                       paste(lubridate::month(exp(fits[[i]]$start_decline_prior)[1]
                                              + min(observed_data[[i]]$date),
                                              label = TRUE,
