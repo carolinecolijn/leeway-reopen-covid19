e471342adc51f05a9351633ccbace87539d0b328
Sean Anderson <sean@seananderson.ca>
2020-08-20 09:57:51 -0700

    Update model-prep.R

--- analysis/model-prep.R
+++ analysis/model-prep.R
@@ -1,12 +1,12 @@
-library(dplyr)
-library(ggplot2)
+library("dplyr")
+library("ggplot2")
 theme_set(ggsidekick::theme_sleek())
-library(covidseir)
-library(purrr)
-library(future)
+library("covidseir")
+library("purrr")
+library("future")
 
-if (packageVersion("covidseir") < "0.0.0.9006") {
-  stop("packageVersion('covidseir') < '0.0.0.9006'.\n",
+if (packageVersion("covidseir") < "0.0.0.9007") {
+  stop("packageVersion('covidseir') < '0.0.0.9007'.\n",
   "Please install the latest version of the model:\n",
   "devtools::install_github('seananderson/covidseir')", call. = FALSE)
 }
@@ -18,12 +18,9 @@ if (!grepl("leeway-reopen-covid19$", wd)) {
 }
 
 options(mc.cores = parallel::detectCores() / 2) # Stan parallel processing
-dir.create("data-generated/",
-  showWarnings = FALSE)
-dir.create("figs/",
-  showWarnings = FALSE)
-dir.create("data-raw",
-  showWarnings = FALSE)
+dir.create("data-generated", showWarnings = FALSE)
+dir.create("figs", showWarnings = FALSE)
+dir.create("data-raw", showWarnings = FALSE)
 
 # For the threshold function; need to get it into the package:
 source("analysis/functions.R")
@@ -32,19 +29,18 @@ this_folder <- "."
 
 make_traceplot <- function(fit) {
   rstan::traceplot(fit$fit,
-    pars = c("R0", "i0", "f_s", "start_decline", "end_decline", "phi", "e"))
+    pars = c("R0", "i0", "f_s", "start_decline", "end_decline", "phi"))
 }
 
 ymd <- lubridate::ymd
 dmy <- lubridate::dmy
 
-ITER <- 400
-CHAINS <- 4
+ITER <- 500
+CHAINS <- 6
 SAMP_FRAC <- 0.2
 i0_PRIOR <- c(log(1), 1)
 
-goog_dat <- readr::read_csv(
-  "data-generated/start-end-google.csv")
+goog_dat <- readr::read_csv("data-generated/start-end-google.csv")
 get_google_start <- function(.region, .dat) {
   .s <- dplyr::filter(goog_dat, region == .region) %>%
     pull(start_date)
