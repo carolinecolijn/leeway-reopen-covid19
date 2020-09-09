e09aa31d54f5d50ddd64e603675c6c9617e4578e
Sean Anderson <sean@seananderson.ca>
2020-09-09 12:30:51 -0700

    Update 08-supp-sensitivity.R

--- analysis/08-supp-sensitivity.R
+++ analysis/08-supp-sensitivity.R
@@ -8,6 +8,10 @@ dat <- observed_data$DE
 SAMP_FRAC <- 0.2
 SENS_ITER <- 300
 
+plot(dat$date, dat$value)
+dat <- dplyr::filter(dat, day %in% fits$DE$days)
+plot(dat$date, dat$value)
+
 sens_fits <- list()
 sens_fits[[1]] <-
   covidseir::fit_seir(
@@ -20,7 +24,9 @@ sens_fits[[1]] <-
     N_pop = 83e6,
     pars = c(D = 4, k1 = 1/5, k2 = 1, q = 0.05, ud = 0.1, ur = 0.02, f0 = 1),
     chains = CHAINS,
-    iter = SENS_ITER
+    iter = SENS_ITER,
+    # fit_type = "optimizing",
+    control = list(adapt_delta = 0.95)
   )
 sens_fits[[2]] <-
   covidseir::fit_seir(
@@ -31,9 +37,12 @@ sens_fits[[2]] <-
     end_decline_prior = c(log(get_google_end("Germany", dat)), 0.1),
     f_seg = make_f_seg(dat),
     N_pop = 83e6,
-    e_prior = c(0.7, 0.025),
+    pars = c(D = 5, k1 = 1/5, k2 = 1, q = 0.05, ud = 0.1, ur = covidseir:::get_ur(0.7, 0.1), f0 = 1),
+    e_prior = c(0.7, 0.05),
     chains = CHAINS,
-    iter = SENS_ITER
+    iter = SENS_ITER,
+    # fit_type = "optimizing",
+    control = list(adapt_delta = 0.95)
   )
 sens_fits[[3]] <-
   covidseir::fit_seir(
@@ -46,22 +55,43 @@ sens_fits[[3]] <-
     N_pop = 83e6,
     pars = c(D = 6, k1 = 1/5, k2 = 1, q = 0.05, ud = 0.1, ur = 0.02, f0 = 1),
     chains = CHAINS,
-    iter = SENS_ITER
+    iter = SENS_ITER,
+    # fit_type = "optimizing",
+    control = list(adapt_delta = 0.95)
   )
+sens_fits[[4]] <- covidseir::fit_seir( # regular fit
+  daily_cases = dat$value,
+  samp_frac_fixed = rep(SAMP_FRAC, nrow(dat)),
+  i0_prior = c(log(5), 1),
+  start_decline_prior = c(log(get_google_start("Germany", dat)), 0.1),
+  end_decline_prior = c(log(get_google_end("Germany", dat)), 0.1),
+  f_seg = make_f_seg(dat),
+  N_pop = 83e6,
+  pars = c(D = 5, k1 = 1/5, k2 = 1, q = 0.05, ud = 0.1, ur = 0.02, f0 = 1),
+  chains = CHAINS,
+  iter = SENS_ITER,
+  # fit_type = "optimizing",
+  control = list(adapt_delta = 0.95)
+)
+
 saveRDS(sens_fits, file = file.path(dg_folder, "fit-sens.rds"))
 sens_fits <- readRDS(file.path(dg_folder, "fit-sens.rds"))
 
-ITER <- 1:200 # downsample for speed
-thresholds_sens <- map(sens_fits, get_thresh, iter = ITER) # subroutine is parallel
+ITER <- 1:150 # downsample for speed
+future::plan(future::multisession)
+thresholds_sens <- map(sens_fits, covidseir::get_threshold, iter = ITER) # subroutine is parallel
 future::plan(future::sequential)
 saveRDS(thresholds_sens, file = file.path(dg_folder, "contact-ratio-thresholds-sens.rds"))
 thresholds_sens <- readRDS(file.path(dg_folder, "contact-ratio-thresholds-sens.rds"))
-thresholds_main <- readRDS(file.path(dg_folder, "contact-ratio-thresholds.rds"))
+# thresholds_main <- readRDS(file.path(dg_folder, "contact-ratio-thresholds.rds"))
 
-sens_fits <- c(sens_fits, list(fits$DE))
-thresholds_sens <- c(thresholds_sens, list(thresholds_main$DE))
-names(sens_fits) <- c("D = 4, e ~ 0.8", "D = 5, e ~ 0.7", "D = 6, e ~ 0.8", "D = 5, e ~ 0.8")
-names(thresholds_sens) <- c("D = 4, e ~ 0.8", "D = 5, e ~ 0.7", "D = 6, e ~ 0.8", "D = 5, e ~ 0.8")
+for (i in 1:4) hist(thresholds_sens[[i]], main = names(thresholds_sens)[[i]], xlim = c(0.2, 0.43))
+for (i in 1:4) hist(sens_fits[[i]]$post$f_s[,1], main = names(thresholds_sens)[[i]], xlim = c(0, 0.3))
+
+# sens_fits <- c(sens_fits, list(fits$DE))
+# thresholds_sens <- c(thresholds_sens, list(thresholds_main$DE))
+names(sens_fits) <- c("D = 4, e ~ 0.83", "D = 5, e ~ 0.7", "D = 6, e ~ 0.83", "D = 5, e ~ 0.83")
+names(thresholds_sens) <- c("D = 4, e ~ 0.83", "D = 5, e ~ 0.7", "D = 6, e ~ 0.83", "D = 5, e ~ 0.83")
 
 f1 <- map(sens_fits, ~ .x$post$f_s[ITER, 1])
 f2 <- map(sens_fits, ~ .x$post$f_s[ITER, 2])
