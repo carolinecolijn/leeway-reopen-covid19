a7f8ad53b0c50e750d3d216bdb349a7f4ffcb0e2
Sean Anderson <sean@seananderson.ca>
2021-01-22 11:39:54 -0800

    Add priors to posterior plot #11

--- analysis/07-supp-estimates.R
+++ analysis/07-supp-estimates.R
@@ -27,16 +27,87 @@ posterior_nonR0 <- filter(posterior,!.variable == 'R0')
 posterior_R0 <- filter(posterior,.variable == 'R0')
 posterior_R0$.value <- getR0(posterior_R0$.value , m)
 
-g <- bind_rows(posterior_nonR0, posterior_R0) %>%
-  mutate(.variable = factor(.variable,
-    levels = c("i0", "R0", "start_decline",
-    "end_decline", "f1", "f2", "e", "phi"))) %>%
-  ggplot(aes(.value)) +
-  geom_histogram() +
-  facet_grid(region ~ .variable, scales = "free") +
-  ylab("Count") +
-  xlab("Posterior parameter value") +
-  coord_cartesian(expand = FALSE)
+# Informative priors to get:
+# i0, R0, start_decline, end_decline, f1, f2, e
 
-ggsave(file.path(fig_folder, "parameter-estimates.pdf"), width = 10, height = 10, plot = g)
-ggsave(file.path(fig_folder, "parameter-estimates.png"), width = 10, height = 10, plot = g)
+get_lnorm_dens <- function(pars, name, lwr = 0, upr = 60, length.out = 400) {
+  x <- seq(lwr, upr, length.out = length.out)
+  tibble(.variable = name, prior_x = x,
+    prior_density = dlnorm(x, meanlog = pars[[1]], sdlog = pars[[2]]))
+}
+get_beta_dens <- function(pars, name, lwr = 0, upr = 1, length.out = 200) {
+  x <- seq(lwr, upr, length.out = length.out)
+  tibble(.variable = name, prior_x = x,
+    prior_density = dbeta(x, shape1 = pars[[1]], shape2 = pars[[2]]))
+}
+
+posteriors <- bind_rows(posterior_nonR0, posterior_R0)
+maxes <- group_by(posteriors, .variable) %>%
+  summarise(.max = max(.value))
+maxes$.max[maxes$.variable == "end_decline"] <- 60
+
+priors <- map_dfr(names(fits), function(i) {
+  m <- fits[[i]]
+  priors <- list()
+  priors[[1]] <- get_lnorm_dens(m$stan_data$i0_prior, "i0", upr = maxes$.max[maxes$.variable == "i0"], length.out = 1000)
+  priors[[2]] <- get_lnorm_dens(m$stan_data$R0_prior, "R0", upr = maxes$.max[maxes$.variable == "R0"])
+  priors[[3]] <- get_lnorm_dens(m$stan_data$start_decline_prior, "start_decline",
+    upr = maxes$.max[maxes$.variable == "start_decline"])
+  priors[[4]] <- get_lnorm_dens(m$stan_data$end_decline_prior, "end_decline", upr = maxes$.max[maxes$.variable == "end_decline"])
+  priors[[5]] <- get_beta_dens(m$stan_data$f_prior[1,], "f1", upr = maxes$.max[maxes$.variable == "f1"])
+  priors[[6]] <- get_beta_dens(m$stan_data$f_prior[2,], "f2", upr = maxes$.max[maxes$.variable == "f2"])
+  priors[[7]] <- get_beta_dens(m$stan_data$e_prior, "e", upr = maxes$.max[maxes$.variable == "e"])
+  priors <- bind_rows(priors) %>% mutate(region = i)
+  priors
+})
+
+# prior_posteriors <- left_join(posterior, priors)
+
+make_post_prior_plot <- function(posterior_dat, prior_dat = NULL, vars) {
+  posterior_dat <- posterior_dat %>%
+    mutate(.variable = factor(.variable,
+      levels = c("i0", "R0", "start_decline",
+        "end_decline", "f1", "f2", "e", "phi")))
+  posterior_dat %>%
+    ggplot(aes(x = .value)) +
+    geom_histogram(aes(y = ..density..), bins = 35L, fill = "grey55") +
+    facet_grid(region ~ .variable, scales = "free") +
+    ylab("Density") +
+    xlab("Parameter value") +
+    coord_cartesian(expand = FALSE)
+    # theme(axis.title.y = element_blank(), axis.title.x = element_blank())
+  if (!is.null(prior_dat)) {
+    prior_dat <- prior_dat %>%
+      mutate(.variable = factor(.variable,
+        levels = c("i0", "R0", "start_decline",
+          "end_decline", "f1", "f2", "e", "phi")))
+    g <- g + geom_line(aes(x = prior_x, y = prior_density), data = prior_dat, inherit.aes = FALSE, lty = 1)
+  }
+  g
+}
+
+g_ef <- make_post_prior_plot(
+  filter(posteriors, .variable %in% c("e", "f1", "f2")),
+  filter(priors, .variable %in% c('e', 'f1', 'f2'))
+  )
+g_decline <- make_post_prior_plot(
+  filter(posteriors, .variable %in% c("start_decline", "end_decline")),
+  filter(priors, .variable %in% c('start_decline', 'end_decline'))
+)
+g_R0 <- make_post_prior_plot(
+  filter(posteriors, .variable %in% c('R0')),
+  filter(priors, .variable %in% c('R0'))
+)
+g_i0 <- make_post_prior_plot(
+  filter(posteriors, .variable %in% c('i0')),
+  filter(priors, .variable %in% c('i0'))
+)
+g_phi <- make_post_prior_plot(
+  filter(posteriors, .variable %in% c('phi'))
+)
+
+g <- cowplot::plot_grid(g_i0, g_R0, g_decline, g_ef, g_phi, nrow = 1L,
+  rel_widths = c(1, 1, 1.5, 1.85, 1))
+
+ggsave(file.path(fig_folder, "parameter-estimates.pdf"), width = 12, height = 10, plot = g)
+ggsave(file.path(fig_folder, "parameter-estimates.png"), width = 12, height = 10, plot = g)
