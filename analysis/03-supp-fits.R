fbaebad2806ceeb7e6264491f129f0bfe3a39c39
Sean Anderson <sean@seananderson.ca>
2021-02-19 18:04:35 -0800

    Add NPI vertical lines

--- analysis/03-supp-fits.R
+++ analysis/03-supp-fits.R
@@ -32,6 +32,21 @@ tidy_projections <- map2(tidy_projections, observed_data, function(pred, obs) {
   mutate(pred, date = seq(first_day, first_day + nrow(pred) - 1, by = "1 day"))
 })
 
+responses <- tibble::tribble(~region, ~mass_gather, ~essential_services,
+  "BC", "2020-03-12", "2020-03-16",
+  "BE", "2020-03-13", "2020-03-18",
+  "CA", "2020-03-11", "2020-03-19",
+  "DE", "2020-03-8",  "2020-03-16",
+  "JP", "2020-03-10", "2020-04-16",
+  "NY", "2020-03-12", "2020-03-22",
+  "NZ", "2020-03-16", "2020-03-25",
+  "ON", "2020-03-13", "2020-03-17",
+  "QC", "2020-03-12", "2020-03-13",
+  "SE", "2020-03-11", "2020-03-26",
+  "UK", "2020-03-16", "2020-03-23",
+  "WA", "2020-03-11", "2020-03-23"
+)
+
 # No projection:
 plots <- pmap(list(fits, tidy_projections, observed_data), function(fit, pred, obs) {
 
@@ -42,6 +57,7 @@ plots <- pmap(list(fits, tidy_projections, observed_data), function(fit, pred, o
   .e1 <- min(obs$date) + quantile(fit$post$end_decline, probs = 0.05) - 1
   .e2 <- min(obs$date) + quantile(fit$post$end_decline, probs = 0.95) - 1
 
+  resp <- dplyr::filter(responses, region == obs$region[1])
   custom_projection_plot(pred_dat = pred, obs_dat = obs) +
     ggtitle(unique(obs$region)) +
     coord_cartesian(
@@ -52,6 +68,8 @@ plots <- pmap(list(fits, tidy_projections, observed_data), function(fit, pred, o
       )
     ) +
     geom_vline(xintercept = ymd("2020-05-01"), lty = 2, col = "grey50", alpha = 0.6) +
+    geom_vline(xintercept = ymd(resp$mass_gather), lty = 3, col = "red", alpha = 0.8) +
+    geom_vline(xintercept = ymd(resp$essential_services), lty = 3, col = "blue", alpha = 0.8) +
     annotate("rect", xmin = .s1, xmax = .s2, ymin = 0, ymax = Inf, fill = "grey50", alpha = 0.5) +
     annotate("rect", xmin = .e1, xmax = .e2, ymin = 0, ymax = Inf, fill = "grey50", alpha = 0.5)
 })
